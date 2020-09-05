5aa4f3be85acf4c4469891cdab744335ad14eb6a
Eric <eric@bostrot.com>
2020-09-05 13:56:29 +0200

    Add donation modal

---
+++ js/donation.js
@@ -0,0 +1,11 @@
+$('#donate').click(function() {
+    $('.modal').show();
+    $("html, body").animate({ scrollTop: 0 }, "slow");
+});
+
+
+$('.modal').bind('click', function (evt) {
+    if(evt.target == $('.modal')[0]) {
+        $('.modal').hide();
+    }
+});
