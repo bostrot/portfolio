6da01f7ecf6e6c7e5b63f512358b02969255d4e7
Eric <eric@bostrot.com>
2020-09-12 16:09:29 +0200

    Fixed CSS padding and formatted GitHub section

--- js/github.js
+++ js/github.js
@@ -15,6 +15,7 @@
         color: #6c757d !important;
     }
     #github_projects a {
+        text-transform: capitalize;
         text-decoration-line: none;
     }
     #github_projects .card {
@@ -29,6 +30,9 @@
         width: 90%;
     }
   `;
+    String.prototype.replaceAt = function(index, replacement) {
+        return this.substr(0, index) + replacement + this.substr(index + replacement.length);
+    }
   fetch("https://api.github.com/users/bostrot/repos")
       .then(response => response.json())
       .then(projects => {
@@ -46,6 +50,7 @@
               if (project != undefined) {
                   if (project.description == null)
                       project.description = "No description.";
+                      project.name = project.name.replaceAll('_', ' ').replaceAll('-', ' ');
                   projectDiv.innerHTML = `
                     <div class="media pt-3">
                         <div class="media-body pb-3 mb-0 lh-125">
