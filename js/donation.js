e16f730161eebe5d51505a639bda8e6ca8028bc5
Eric <me@bostrot.com>
2022-02-11 02:19:06 +0100

    Fix donate modal

--- js/donation.js
+++ js/donation.js
@@ -1,12 +1,34 @@
-$('#donate').click(function () {
-    $('.modal').show();
-    $("html, body").animate({
-        scrollTop: 0
-    }, "slow");
-});
 
-$('.modal').bind('click', function (evt) {
-    if (evt.target == $('.modal')[0]) {
-        $('.modal').hide();
-    }
-});
+    let donateDiv = document.createElement("div");
+    donateDiv.innerHTML = `
+        <!-- Modal -->
+        <div class="modal donate-modal hidden">
+            <div class="card">
+                <h2>Want to help me create more awesome projects?<br />Donate now &hearts;</h2>
+                <div class="icon-area">
+                    <a href="https://www.patreon.com/erictrenkel" onclick="plausible('patreon')" target="_blank"><i
+                            class="fab fa-patreon fa-3x"></i></a>
+                    <a href="http://paypal.me/bostrot" onclick="plausible('paypal')" target="_blank"><i
+                            class="fab fa-paypal fa-3x"></i></a>
+                    <a href="https://github.com/sponsors/bostrot" onclick="plausible('github-sponsors')" target="_blank"><i
+                            class="fab fa-github fa-3x"></i></a>
+                </div>
+            </div>
+        </div>
+    `;
+    document.getElementsByTagName("html")[0].appendChild(donateDiv);
+    let donateBtn = document.getElementById('donate');
+    let donateModal = document.getElementsByClassName('donate-modal')[0];
+    donateBtn.onclick = function () {
+        donateModal.classList.remove("hidden");
+        document.body.scrollTop = 0; // For Safari
+        document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+        document.body.style.opacity = 0.2;
+    };
+
+    donateModal.onclick = function (evt) {
+        if (evt.target == donateModal) {
+            donateModal.classList.add("hidden");
+            document.body.style.opacity = 1;
+        }
+    };
--- js/github.js
+++ js/github.js
@@ -2,39 +2,38 @@
       let script = document.createElement("script");
       script.src = "https://cdnjs.cloudflare.com/ajax/libs/showdown/1.9.1/showdown.min.js";
       document.body.appendChild(script);
-  };
 
-  // Lambda sort factory function
-  const sortOnKey = (key, desc) => {
-      return (a, b) => {
-          a = a[key];
-          b = b[key];
-          return desc ? b - a : a - b;
+      // Lambda sort factory function
+      const sortOnKey = (key, desc) => {
+          return (a, b) => {
+              a = a[key];
+              b = b[key];
+              return desc ? b - a : a - b;
+          }
+      };
+
+      String.prototype.replaceAt = function (index, replacement) {
+          return this.substr(0, index) + replacement + this.substr(index + replacement.length);
       }
-  };
-
-  String.prototype.replaceAt = function (index, replacement) {
-      return this.substr(0, index) + replacement + this.substr(index + replacement.length);
-  }
-  fetch("https://api.github.com/users/bostrot/repos?per_page=100")
-      .then(response => response.json())
-      .then(projects => {
-          // Sort
-          projects = projects.sort(sortOnKey("stargazers_count", true));
-          // Inserting
-          let projectsDiv = document.getElementById("github_projects");
-          // Empty div
-          projectsDiv.innerHTML = "";
-          projectsDiv.classList = "";
-          for (let i = 0; i < 6; i++) {
-              let projectDiv = document.createElement("div");
-              projectDiv.classList = "card hoverable";
-              let project = projects[i];
-              if (project != undefined) {
-                  if (project.description == null)
-                      project.description = "No description.";
-                  project.name = project.name.replaceAll('_', ' ').replaceAll('-', ' ');
-                  projectDiv.innerHTML = `
+      fetch("https://api.github.com/users/bostrot/repos?per_page=100")
+          .then(response => response.json())
+          .then(projects => {
+              // Sort
+              projects = projects.sort(sortOnKey("stargazers_count", true));
+              // Inserting
+              let projectsDiv = document.getElementById("github_projects");
+              // Empty div
+              projectsDiv.innerHTML = "";
+              projectsDiv.classList = "";
+              for (let i = 0; i < 6; i++) {
+                  let projectDiv = document.createElement("div");
+                  projectDiv.classList = "card hoverable";
+                  let project = projects[i];
+                  if (project != undefined) {
+                      if (project.description == null)
+                          project.description = "No description.";
+                      project.name = project.name.replaceAll('_', ' ').replaceAll('-', ' ');
+                      projectDiv.innerHTML = `
                     <div class="media">
                         <div class="media-body">
                             <a href="${project.html_url}" onclick="plausible('${project.name}')" target="_blank">
@@ -48,15 +47,15 @@
                         <p>${project.description}</p>
                     </div>
                     `;
-                  fetch(`https://raw.githubusercontent.com/${project.full_name}/master/README.md`)
-                      .then(response => response.text())
-                      .then(text => {
-                          // Convert markdown to html
-                          let converter = new showdown.Converter();
-                          let html = converter.makeHtml(text);
-                          let projectModal = document.createElement("div");
-                          projectModal.classList = "modal github-modal hidden";
-                          projectModal.innerHTML = `
+                      fetch(`https://raw.githubusercontent.com/${project.full_name}/master/README.md`)
+                          .then(response => response.text())
+                          .then(text => {
+                              // Convert markdown to html
+                              let converter = new showdown.Converter();
+                              let html = converter.makeHtml(text);
+                              let projectModal = document.createElement("div");
+                              projectModal.classList = "modal github-modal hidden";
+                              projectModal.innerHTML = `
                         <div class="card">
                             <a href="${project.html_url}" target="blank" style="float: right">Visit on GitHub</a>
                             <br>
@@ -65,22 +64,23 @@
                         </div>
                     `;
 
-                          projectDiv.onclick = function () {
-                              projectModal.classList.remove("hidden");
-                              document.body.scrollTop = 0; // For Safari
-                              document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-                              document.body.style.opacity = 0.2;
-                          };
+                              projectDiv.onclick = function () {
+                                  projectModal.classList.remove("hidden");
+                                  document.body.scrollTop = 0; // For Safari
+                                  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+                                  document.body.style.opacity = 0.2;
+                              };
 
-                          projectModal.onclick = function (evt) {
-                              if (evt.target == projectModal) {
-                                  projectModal.classList.add("hidden");
-                                  document.body.style.opacity = 1;
-                              }
-                          };
-                          document.getElementsByTagName("html")[0].appendChild(projectModal);
-                      });
-                  projectsDiv.appendChild(projectDiv);
+                              projectModal.onclick = function (evt) {
+                                  if (evt.target == projectModal) {
+                                      projectModal.classList.add("hidden");
+                                      document.body.style.opacity = 1;
+                                  }
+                              };
+                              document.getElementsByTagName("html")[0].appendChild(projectModal);
+                          });
+                      projectsDiv.appendChild(projectDiv);
+                  }
               }
-          }
-      });
+          });
+  };
